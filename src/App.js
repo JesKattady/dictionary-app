c71fcc733b3a6b506e07cadf9434cb831ac1c2c2
JesKattady <jeskattady98@gmail.com>
2022-02-01 16:55:52 +0000

    Setup and cleaned

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./owl2.jpg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+      <img src={logo} alt="logo" className="logo img-fluid" />
+      <span className="heading"> Dictionary </span>
+    </div>
+  );
+}
+
+export default App;
