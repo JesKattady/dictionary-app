fa1517a61b497a2ac0052c7bca71a9c657d720d8
JesKattady <jeskattady98@gmail.com>
2022-02-02 11:38:42 +0000

    Added search form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import logo from "./owl2.jpg";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
-      <img src={logo} alt="logo" className="logo img-fluid" />
-      <span className="heading"> Dictionary </span>
+      <header className="app-header">
+        <img src={logo} alt="logo" className="logo img-fluid" />
+        <span className="heading"> Dictionary </span>
+      </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="text-center">
+        <small> Coded by Jesna</small>
+      </footer>
     </div>
   );
 }
---
+++ src/Dictionary.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import "./Dictionary.css";
+
+export default function Dictionary() {
+  let [keyword, setKeyword] = useState("");
+  function search(event) {
+    event.preventDefault();
+    alert(`searching for ${keyword}`);
+  }
+  function handleKeyword(event) {
+    setKeyword(event.target.value);
+  }
+  return (
+    <div className="Dictionary">
+      <form onSubmit={search}>
+        <input
+          type="search"
+          placeholder="search for a word"
+          autoFocus={true}
+          onChange={handleKeyword}
+        />
+      </form>
+    </div>
+  );
+}
