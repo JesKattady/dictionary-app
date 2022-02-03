dab268e3e4b4159b49a597d4d84f5e7be3327f2a
JesKattady <jeskattady98@gmail.com>
2022-02-03 12:49:41 +0000

    completed rendering the word definition

--- src/Results.js
+++ src/Results.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Meaning from "./Meaning";
 import "./Dictionary.css";
 
 export default function Results(props) {
@@ -9,7 +10,11 @@ export default function Results(props) {
         <h1>{props.data.word}</h1>
         <h2>{props.data.phonetic}</h2>
         {props.data.meanings.map(function (meaning, index) {
-          return meaning.definitions[0].definition;
+          return (
+            <div key={index}>
+              <Meaning meaning={meaning} />
+            </div>
+          );
         })}
       </div>
     );
